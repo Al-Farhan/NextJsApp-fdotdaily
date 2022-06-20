c5df1fd74477fb936951c61684808a2ea0c8a5bf
FARHAN SHAIKH <programmingwithfarhan@gmail.com>
2022-06-20 19:15:53 +0530

    Final push
    We have completed blog website.

--- fdotdaily/pages/blogpost/[slug].js
+++ fdotdaily/pages/blogpost/[slug].js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogPost.module.css';
+import * as fs from 'node:fs';
 
-const slug = () => {
-    const router = useRouter();
-    const { slug } = router.query;
-    console.log(router.query);
-    return (
-        <div>{slug}</div>
-    )
+const Slug = (props) => {
+  function createMarkup(c) {
+  return {__html: c};
+}
+  const [blog, setBlog] = useState(props.myBlog);
+
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>{blog && blog.title}</h1>
+        <hr />
+        {blog && <div dangerouslySetInnerHTML={createMarkup(blog.content)}></div>}
+      </main>
+    </div>
+  )
 }
 
-export default slug
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug:'how-to-learn-js' } },
+      { params: { slug:'how-to-learn-nextjs' } }
+    ],
+    fallback: true // false or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  
+
+  let myBlog = await fs.promises.readFile(`blogdata/${slug}.json`,'utf-8')
+
+  return {
+    props: { myBlog: JSON.parse(myBlog) }, // will be passed to the page component as props
+  }
+}
+
+export default Slug
--- fdotdaily/pages/contact.js
+++ fdotdaily/pages/contact.js
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../styles/Contact.module.css'
 
 const Contact = () => {
+  const [name, setname] = useState('')
+  const [email, setemail] = useState('')
+  const [phone, setphone] = useState('')
+  const [desc, setdesc] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, email, phone, desc };
+
+    fetch('http://localhost:3000/api/postcontact', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.text())
+      .then(data => {
+        
+        alert("Thanks for contacting us");
+        setphone('')
+        setname('')
+        setdesc('')
+        setemail('')
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setname(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setemail(e.target.value)
+    }
+    else if (e.target.name == 'phone') {
+      setphone(e.target.value)
+    }
+    else if (e.target.name == 'desc') {
+      setdesc(e.target.value)
+    }
+  }
   return (
-    <div>This is contact page</div>
+    <div className={styles.container}>
+      <h1>Contact us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label for="name" className={styles.formlabel}>Enter your name</label>
+          <input className={styles.input} type="text" value={name} onChange={handleChange}  id="name" aria-describedby="emailHelp" name='name' required/>
+        </div>
+
+        <div className={styles.mb3}>
+          <label for="email" className={styles.formlabel}>Email address</label>
+          <input className={styles.input} type="email" value={email} onChange={handleChange} id="email" aria-describedby="emailHelp" name='email' required/>
+          <div id="emailHelp" className={styles.formtext}>We'll never share your email with anyone else.</div>
+        </div>
+        <div className={styles.mb3}>
+          <label for="phone" className={styles.formlabel}>Enter your phone</label>
+          <input className={styles.input} type="phone" value={phone} onChange={handleChange} id="phone" aria-describedby="emailHelp" name='phone' required/>
+        </div>
+        <div className={styles.mb3}>
+          <label className={styles.formlabel} for="desc">Concern</label>
+          <textarea className={styles.input} value={desc} htmlFor="desc" onChange={handleChange} placeholder="Write your concern here" id="desc" name='desc' required/> 
+        </div>
+        <button type="submit" className={styles.btn}>Submit</button>
+      </form>
+    </div>
   )
 }
 
